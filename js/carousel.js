8a29ee4af0e82f082f9c7166897494f94cd2c7a0
Dominic Rathbone <drathbone@ln-mowifi-02-10.1.30.26.net.bton.ac.uk>
2015-11-17 22:00:21 +0000

    finished website structure and design

--- js/carousel.js
+++ js/carousel.js
@@ -2,5 +2,11 @@ $(document).ready(function(){
   $('.carousel').slick({
     dots: true,
     speed: 300,
+    adaptiveHeight: true,
+    arrows: true
   });
+});
+
+$('#start-button').click(function(){
+    $(".carousel").slick('slickNext');
 });
